ccabd9819b1097e4539c77ed5f64ef61f8872479
Joelnicolass <joelnicolass@gmail.com>
2023-09-14 20:18:07 -0300

    sound

--- src/components/Audio/Audio.jsx
+++ src/components/Audio/Audio.jsx
@@ -1,17 +1,42 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import useSound from "use-sound";
 
-const Audio = () => {
-  /* const [play] = useSound("/song.mp3", {
+const AudioPlayer = () => {
+  const isLoaded = useRef(false);
+  const isPlaying = useRef(false);
+
+  const [play] = useSound("/song.mp3", {
     volume: 0.5,
     interrupt: true,
+    onload: () => {
+      isLoaded.current = true;
+      console.log("loaded");
+    },
   });
 
   useEffect(() => {
-    play();
-  }, [play]); */
+    if (!isLoaded.current) return;
+
+    const refmouse = document.addEventListener("mousemove", () => {
+      if (isPlaying.current) return;
+      play();
+
+      isPlaying.current = true;
+    });
+
+    const refscroll = document.addEventListener("scroll", () => {
+      if (isPlaying.current) return;
+      play();
+
+      isPlaying.current = true;
+    });
+
+    return () => {
+      document.removeEventListener("click", refmouse);
+    };
+  }, [isLoaded, play]);
 
   return (
     <div
@@ -24,10 +49,8 @@ const Audio = () => {
         color: "white",
         fontSize: "1.5rem",
       }}
-    >
-      Audio
-    </div>
+    ></div>
   );
 };
 
-export default Audio;
+export default AudioPlayer;
