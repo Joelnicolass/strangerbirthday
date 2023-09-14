e21b4a97a1296e455113a85a7f96f0f58a30f54f
Joelnicolass <joelnicolass@gmail.com>
2023-09-14 16:45:34 -0300

    fix

--- src/components/AnserQuiz/AnswerQuiz.jsx
+++ src/components/AnserQuiz/AnswerQuiz.jsx
@@ -25,12 +25,10 @@ const AnswersQuiz = ({
           <motion.div
             initial={{
               opacity: 0,
-              scale: 0.9,
               y: 20,
             }}
             animate={{
               opacity: 1,
-              scale: 1,
               y: 0,
             }}
             transition={{
